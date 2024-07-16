b0aa490fa183f018d93790e2db28ae7079555b6a
rongrong77 <132054978+rongrong77@users.noreply.github.com>
2024-07-17 02:22:30 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import torch
 import numpy as np
 import os
 from torch.utils.data.dataloader import DataLoader
-from tslearn.metrics.cysax import LinearRegression
 from config import get_config_universal
 from dataset import DataSet
 from datasetbuilder import DataSetBuilder
@@ -16,6 +15,8 @@ from visualization.wandb_plot import wandb_plotly_true_pred
 import pickle
 from model.transformer_tsai import TransformerTSAI
 from sklearn.preprocessing import StandardScaler
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 
@@ -35,56 +36,35 @@ def run_main():
     dataset_handler = DataSet(config, load_dataset=True)
 
     kihadataset_train, kihadataset_test = dataset_handler.run_dataset_split()
-    # split_data = {
-    #     'x': kihadataset_test['x'],
-    #     'y': kihadataset_test['y'],
-    #     'labels': kihadataset_test['labels']
-    # }
-    # with open('split_data.pkl', 'wb') as f:
-    #     pickle.dump(split_data, f)
 
-    kihadataset_test['x'], kihadataset_test['y'], kihadataset_test['labels'] = dataset_handler.run_segmentation(kihadataset_test['x'],kihadataset_test['y'], kihadataset_test['labels'])
-    # segmented_data = {
-    #     'x': kihadataset_test['x'],
-    #     'y': kihadataset_test['y'],
-    #     'labels': kihadataset_test['labels']
-    # }
-    # with open('segmented_data2.pkl', 'wb') as f:
-    #     pickle.dump(segmented_data, f)
+    train_size = 0.8
+    num_train = int(train_size * len(kihadataset_train['x']))
+    train_indices = np.random.choice(range(len(kihadataset_train['x'])), size=num_train, replace=False)
+    val_indices = list(set(range(len(kihadataset_train['x']))) - set(train_indices))
+    
+    kihadataset_val = {
+        'x': [kihadataset_train['x'][i] for i in val_indices],
+        'y': [kihadataset_train['y'][i] for i in val_indices],
+        'labels': kihadataset_train['labels'].iloc[val_indices].reset_index(drop=True)
+    }
+    kihadataset_train = {
+        'x': [kihadataset_train['x'][i] for i in train_indices],
+        'y': [kihadataset_train['y'][i] for i in train_indices],
+        'labels': kihadataset_train['labels'].iloc[train_indices].reset_index(drop=True)
+    }
+
+    
+    
+    kihadataset_train['x'], kihadataset_train['y'], kihadataset_train['labels'] = dataset_handler.run_segmentation(kihadataset_train['x'], kihadataset_train['y'], kihadataset_train['labels'])
+    kihadataset_val['x'], kihadataset_val['y'], kihadataset_val['labels'] = dataset_handler.run_segmentation(kihadataset_val['x'], kihadataset_val['y'], kihadataset_val['labels'])
+    kihadataset_test['x'], kihadataset_test['y'], kihadataset_test['labels'] = dataset_handler.run_segmentation(kihadataset_test['x'], kihadataset_test['y'], kihadataset_test['labels'])
    
     if tuning == True:
         del kihadataset_test
         ParameterTuning(config, kihadataset_train)
-    else:
-        kihadataset_train['x'], kihadataset_train['y'], kihadataset_train['labels'] = dataset_handler.run_segmentation(
-            kihadataset_train['x'],
-            kihadataset_train['y'], kihadataset_train['labels'])
         
-
-        if config['model_name'] == 'linear':
-            x_train = kihadataset_train['x'][:, :, :]
-            y_train = kihadataset_train['y'][:, :, :]
-            x_tr = np.reshape(x_train, [x_train.shape[0], x_train.shape[1]*x_train.shape[2]])
-            y_tr = np.reshape(y_train, [y_train.shape[0], y_train.shape[1]*y_train.shape[2]])
-            x_test = kihadataset_test['x'][:, :, :]
-            y_test = kihadataset_test['y'][:, :, :]
-            x_true = np.reshape(x_test, [x_test.shape[0], x_test.shape[1] * x_test.shape[2]])
-            y_true = y_test
-            # config['model_train_activity'], config['model_test_activity'] = get_model_name_from_activites(
-            #     config['train_activity'], config['test_activity'])
-            # model_file = config['model_name'] + '_' + "".join(config['model_train_activity']) + \
-            #              '_' + "".join(config['model_test_activity']) + '.pt'
-            model_file = 'a.pt'
-            if load_model and os.path.isfile('./caches/trained_model/' + model_file):
-                model = torch.load(os.path.join('./caches/trained_model/', model_file))
-            else:
-                model = LinearRegression().fit(x_tr, y_tr)
-                if save_model:
-                    torch.save(model, os.path.join('./caches/trained_model/', model_file))
-
-            y_pred = model.predict(x_true)
-            y_pred = np.reshape(y_pred, [y_test.shape[0], y_test.shape[1], y_test.shape[2]])
-        elif config['model_name'] == 'transformer':
+    else:
+        if config['model_name'] == 'transformer':
                 
             # Transformer model
             c_in = kihadataset_train['x'].shape[2]
@@ -103,22 +83,21 @@ def run_main():
                 optimizer = torch.optim.Adam(transformer_model.parameters(), lr=config['learning_rate'])
                 criterion = torch.nn.MSELoss()
                 num_epochs = config['n_epoch']
+                train_losses = []
+                val_losses = []
 
-            # Prepare the dataloaders
+                # Prepare the dataloaders
                 train_dataset = DataSetBuilder(kihadataset_train['x'], kihadataset_train['y'], kihadataset_train['labels'],
                                             transform_method=config['data_transformer'], scaler=None)
                 train_dataloader = DataLoader(dataset=train_dataset, batch_size=config['batch_size'], shuffle=True)
                 
-                with open('train_dataset.pkl', 'wb') as f:
-                    pickle.dump({
-                        'x': train_dataset.x.numpy(),
-                        'y': train_dataset.y.numpy(),
-                        'labels': train_dataset.labels
-                    }, f)
-                print("Train dataset saved to train_dataset.pkl")
-
+                val_dataset = DataSetBuilder(kihadataset_val['x'], kihadataset_val['y'], kihadataset_val['labels'],
+                                            transform_method=config['data_transformer'], scaler=train_dataset.scaler)
+                val_dataloader = DataLoader(dataset=val_dataset, batch_size=config['batch_size'], shuffle=False)
+               
                 for epoch in range(num_epochs):
                     transformer_model.train()
+                    epoch_loss = 0
                     for x_batch, y_batch in train_dataloader:
                         x_batch = x_batch.float()  # Convert to float32
                         y_batch = y_batch.float()  # Convert to float32
@@ -127,38 +106,69 @@ def run_main():
                         loss = criterion(y_pred, y_batch)
                         loss.backward()
                         optimizer.step()
+                    epoch_loss /= len(train_dataloader)
+                    train_losses.append(epoch_loss)
                     print(f'Epoch {epoch+1}/{num_epochs}, Loss: {loss.item()}')
 
+                    if (epoch + 1) % 10 == 0:
+                        transformer_model.eval()
+                        val_loss = 0
+                        with torch.no_grad():
+                            for x_batch, y_batch in val_dataloader:
+                                x_batch = x_batch.float()
+                                y_batch = y_batch.float()
+                                y_pred = transformer_model(x_batch)
+                                loss = criterion(y_pred, y_batch)
+                                val_loss += loss.item()
+                        val_loss /= len(val_dataloader)
+                        val_losses.append(val_loss)
+                        print(f'Epoch {epoch+1}/{num_epochs}, Validation Loss: {val_loss}')
 
-            # Evaluate the transformer model
-            transformer_model.eval()
-            test_dataset = DataSetBuilder(kihadataset_test['x'], kihadataset_test['y'], kihadataset_test['labels'],
-                                        transform_method=config['data_transformer'], scaler=train_dataset.scaler)
-            test_dataloader = DataLoader(dataset=test_dataset, batch_size=config['batch_size'], shuffle=False)
-            y_pred_list = []
-            y_true_list = []
+                        # Save checkpoint every 10 epochs
+                        checkpoint_path = os.path.join('./caches/trained_model/', f'checkpoint_epoch_{epoch+1}.pt')
+                        torch.save(transformer_model.state_dict(), checkpoint_path)
+                        print(f"Checkpoint saved at {checkpoint_path}")
 
-            with open('test_dataset.pkl', 'wb') as f:
-                    pickle.dump({
-                        'x': test_dataset.x.numpy(),
-                        'y': test_dataset.y.numpy(),
-                        'labels': test_dataset.labels
-                    }, f)
-            print("Train dataset saved to test_dataset.pkl")
+                #Plot the loss
+                plt.figure()
+                plt.plot(range(1, num_epochs + 1), train_losses, label='Training Loss')
+                plt.xlabel('Epoch')
+                plt.ylabel('Loss')
+                plt.title('Training Loss Over Epochs')
+                plt.legend()
+                plt.savefig('training_loss_plot.png')
+                plt.show()
+                print("Training loss plot saved as training_loss_plot.png")
+                # Evaluate the transformer model
+                
+                transformer_model.eval()
+                test_dataset = DataSetBuilder(kihadataset_test['x'], kihadataset_test['y'], kihadataset_test['labels'],
+                                            transform_method=config['data_transformer'], scaler=train_dataset.scaler)
+                test_dataloader = DataLoader(dataset=test_dataset, batch_size=config['batch_size'], shuffle=False)
+                y_pred_list = []
+                y_true_list = []
 
-            with torch.no_grad():
-                for x_batch, y_batch in test_dataloader:
-                    x_batch = x_batch.float()  
-                    y_batch = y_batch.float() 
-                    y_pred = transformer_model(x_batch)
-                    y_pred_list.append(y_pred)
-                    y_true_list.append(y_batch)
+                with open('test_dataset.pkl', 'wb') as f:
+                        pickle.dump({
+                            'x': test_dataset.x.numpy(),
+                            'y': test_dataset.y.numpy(),
+                            'labels': test_dataset.labels
+                        }, f)
+                print("Train dataset saved to test_dataset.pkl")
 
-            y_pred = torch.cat(y_pred_list, dim=0).numpy()
-            y_true = torch.cat(y_true_list, dim=0).numpy()
+                with torch.no_grad():
+                    for x_batch, y_batch in test_dataloader:
+                        x_batch = x_batch.float()  
+                        y_batch = y_batch.float() 
+                        y_pred = transformer_model(x_batch)
+                        y_pred_list.append(y_pred)
+                        y_true_list.append(y_batch)
 
-            if save_model:
-                    torch.save(transformer_model.state_dict(), os.path.join('./caches/trained_model/', 'final_transformer_model.pt'))
+                y_pred = torch.cat(y_pred_list, dim=0).numpy()
+                y_true = torch.cat(y_true_list, dim=0).numpy()
+
+                if save_model:
+                        torch.save(transformer_model.state_dict(), os.path.join('./caches/trained_model/', 'final_transformer_model(APIA).pt'))
 
         else:
             # Classification
